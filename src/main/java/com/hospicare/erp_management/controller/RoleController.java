2b78c9b3f0298e2218b8e3eee8c1a26c6c0e6f57
Jose Luis Rico <joseluis.rico@mcimanager.com>
2024-11-28 14:08:01 +0100

    Auth working

--- src/main/java/com/hospicare/erp_management/controller/RoleController.java
+++ src/main/java/com/hospicare/erp_management/controller/RoleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/roles")
+@RequestMapping("/admin/roles")
 public class RoleController {
 
     @Autowired
--- src/main/java/com/hospicare/erp_management/service/OurUserDetailsService.java
+++ src/main/java/com/hospicare/erp_management/service/OurUserDetailsService.java
@@ -2,11 +2,13 @@ package com.hospicare.erp_management.service;
 
 
 import com.hospicare.erp_management.repository.UsersRepo;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OurUserDetailsService implements UserDetailsService {
@@ -14,8 +16,16 @@ public class OurUserDetailsService implements UserDetailsService {
     @Autowired
     private UsersRepo usersRepo;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usersRepo.findByEmail(username).orElseThrow();
+        UserDetails userDetails = usersRepo.findByEmail(username).orElseThrow();
+
+        entityManager.refresh(userDetails);
+
+        return userDetails;
     }
 }
